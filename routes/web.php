cc47fe5cc596bc7235300009db9baa18ef15ce70
aqilat94 <aqilat94@gmail.com>
2021-07-30 12:21:40 +0800

    add route for crud payment gateway

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Purchase;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +21,31 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/return-url', function (Request $request){
+    // dd($request->all());
+    $purchase = Purchase::where('toyyibpay_bill_code', $request->billcode)->first();
+    if($purchase)
+    {
+        if ($purchase->id == $request->order_id)
+        {
+            $purchase->update(['payment_status' => 1]);
+
+            return 'Thank you for your purchase';
+        }
+        return 'response is not valid';
+    }
+    else
+    {
+        return 'please check your';
+    }
+});
+
+Route::post('/callback-url', function (Request $request){
+    dd($request->all());
+});
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/purchase/phone',[App\Http\Controllers\PurchaseController::class, 'index']);
+Route::get('/buy/laptop/{phone}',[App\Http\Controllers\PurchaseController::class, 'store'])->name('store');
+Route::get('/make/purchase', [App\Http\Controllers\PurchaseController::class, 'show'])->name('show');
+Route::get('/show/bill/{purchase}', [App\Http\Controllers\PurchaseController::class, 'showBill'])->name('showBill');
