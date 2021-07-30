6792bad78f7b97ec7092d2f8177f8750050e871c
aqilat94 <aqilat94@gmail.com>
2021-07-30 12:18:12 +0800

    Model for purchase

---
+++ app/Models/Purchase.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Purchase extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'phone_id',
+        'price',
+        'toyyibpay_bill_code',
+        'payment_status'
+    ];
+    
+}
