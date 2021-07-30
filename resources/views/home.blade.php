cf2b5c5b53a7be79896075c562e5d152f68620d9
aqilat94 <aqilat94@gmail.com>
2021-07-30 12:22:00 +0800

    blade to display purchase

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">Laptop</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,10 +14,67 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                   <div class="card-body">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Phone Brand</th>
+                                    <th>Phone Model</th>
+                                    <th>Price</th>
+                                    <th>Action</th>
+                                </tr>
+
+                            </thead>
+                            <tbody>
+                            @foreach ($phones as $key=>$phone)
+                                <tr>
+                                    <td>{{$phone->brand}}</td>
+                                    <td>{{$phone->model}}</td>
+                                    <td>RM {{$phone->price}}</td>
+                                    <td>
+                                        <a href="{{route('store',$phone)}}" class="btn btn-primary" onclick="">Buy</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                        <div class="card-body">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>Price</th>
+                                        <th>Payment Status</th>
+                                        <th>Action</th>
+                                    </tr>
+    
+                                </thead>
+                                <tbody>
+                                @foreach ($purchases as $key=>$purchase)
+                                    <tr>
+                                        <td>RM {{$purchase->price}}</td>
+                                        <td>
+                                            @if ($purchase->payment_status == 0)
+                                                <a>Not Paid</a>
+                                            @else
+                                                <a>Paid</a>
+                                            @endif
+                                        </td>
+                                        @if ($purchase->payment_status == 0)
+                                        <td>
+                                            <a href="{{route('showBill',$purchase)}}" class="btn btn-warning" onclick="">Buy</a>
+                                        </td>
+                                        @else
+                                        <td>No Action</td>
+                                        @endif
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                   </div>
                 </div>
             </div>
         </div>
     </div>
+    <passport-token></passport-token>
 </div>
 @endsection
